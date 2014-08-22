7778678f5e4443bd6213a0ad83821cfe503f416f
Andy Matuschak <andy@khanacademy.org>
2014-08-21 21:30:08 -0700

    Access qualifiers for tunable spec internal KFTunableSpec property

--- BlockPhysics/TunableSpec.swift
+++ BlockPhysics/TunableSpec.swift
@@ -10,51 +10,51 @@ import Foundation
 import UIKit
 
 class TunableSpec {
-	var _spec: KFTunableSpec!
+	private var spec: KFTunableSpec!
 	init(name: String) {
-		_spec = KFTunableSpec.specNamed(name) as KFTunableSpec?
-		assert(_spec != nil, "failed to load spec named \(name)")
+		spec = KFTunableSpec.specNamed(name) as KFTunableSpec?
+		assert(spec != nil, "failed to load spec named \(name)")
 	}
 
 	subscript(key: String) -> Double {
-		return _spec.doubleForKey(key)
+		return spec.doubleForKey(key)
 	}
 
 	subscript(key: String) -> CGFloat {
-		return CGFloat(_spec.doubleForKey(key))
+		return CGFloat(spec.doubleForKey(key))
 	}
 
 	subscript(key: String) -> Bool {
-		return _spec.boolForKey(key)
+		return spec.boolForKey(key)
 	}
 
 	func withKey<T where T: AnyObject>(key: String, owner weaklyHeldOwner: T, maintain maintenanceBlock: (T, Double) -> ()) {
-		_spec.withDoubleForKey(key, owner: weaklyHeldOwner, maintain: { maintenanceBlock($0 as T, $1) })
+		spec.withDoubleForKey(key, owner: weaklyHeldOwner, maintain: { maintenanceBlock($0 as T, $1) })
 	}
 
 	func withKey<T where T: AnyObject>(key: String, owner weaklyHeldOwner: T, maintain maintenanceBlock: (T, CGFloat) -> ()) {
-		_spec.withDoubleForKey(key, owner: weaklyHeldOwner, maintain: { maintenanceBlock($0 as T, CGFloat($1)) })
+		spec.withDoubleForKey(key, owner: weaklyHeldOwner, maintain: { maintenanceBlock($0 as T, CGFloat($1)) })
 	}
 
 	func withKey<T where T: AnyObject>(key: String, owner weaklyHeldOwner: T, maintain maintenanceBlock: (T, Bool) -> ()) {
-		_spec.withBoolForKey(key, owner: weaklyHeldOwner, maintain: { maintenanceBlock($0 as T, $1) })
+		spec.withBoolForKey(key, owner: weaklyHeldOwner, maintain: { maintenanceBlock($0 as T, $1) })
 	}
 
 	// boo, it has to be an NSDictionary because the spec value types are heterogeneous
 	var dictionaryRepresentation: NSDictionary {
-		return _spec.dictionaryRepresentation()
+		return spec.dictionaryRepresentation()
 	}
 
 	var twoFingerTripleTapGestureRecognizer: UIGestureRecognizer {
-		return _spec.twoFingerTripleTapGestureRecognizer()
+		return spec.twoFingerTripleTapGestureRecognizer()
 	}
 
 	var controlsAreVisible: Bool {
 		get {
-			return _spec.controlsAreVisible
+			return spec.controlsAreVisible
 		}
 		set {
-			_spec.controlsAreVisible = newValue
+			spec.controlsAreVisible = newValue
 		}
 	}
 }
